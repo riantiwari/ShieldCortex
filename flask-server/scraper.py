366818cc29b209e36b8d5d6202064d39d619a8a5
RianTiwari1 <64282367+riantiwari@users.noreply.github.com>
2024-04-20 16:14:12 -0400

    combining method and main

--- flask-server/scraper.py
+++ flask-server/scraper.py
@@ -36,15 +36,60 @@ def main(headers):
     #     print(link.get('href'))
     
 
+#contracts = []
+#for link in links:
+    #conArr = scrape_contracts(link)
+    #for con in conArr:
+        #contracts.append(con)
+#csv_file = open('contracts.csv', 'w', encoding='utf-8', newline='')
+# initializing the writer object to insert data
+# in the CSV file
+# writer = csv.writer(csv_file)
 
-def scrape_page(url):
-    scrapePara = requests.get(url, headers=headers)
-    paraSoup = BeautifulSoup(scrapePara.text, "html.parser")
-    paraList = paraSoup.find_all('p')
-    for para in paraList:
-        print(para)
-        print()
+# # writing the header of the CSV file
+# writer.writerow(['Text', 'Author', 'Tags'])
+
+# # writing each row of the CSV
+# for quote in quotes:
+#     writer.writerow(quote.values())
+
+# # terminating the operation and releasing the resources
+# csv_file.close()
+
+"""
+Function to scrape text content from paragraph tags within the body of the page highlighting each
+of the contracts of the DOD on a certain date.
+
+Args:
+    url (str): The URL of the web page to be scraped.
+
+Returns:
+    list: A list of strings, each representing the text content of a paragraph tag.
+"""
+def scrape_contracts(url):
+    # Send an HTTP request to the provided URL and store the response.
+    scrapeCon = requests.get(url, headers=headers)
+
+    # Parse the HTML content of the response using Beautiful Soup with the 'html.parser'.
+    conSoup = BeautifulSoup(scrapeCon.text, "html.parser")
+    
+    # Find all paragraph ('<p>') elements that are within the '<body>' of the HTML document.
+    conList = conSoup.find("body").find_all("p")
+    
+    # Iterate through each paragraph element found.
+    for con in conList:
+        # Extract and store the text content of the paragraph, removing HTML tags.
+        con = con.get_text()
         
+        # # Print the text content of the paragraph to the console.
+        # print(con)
+        
+        # # Print an empty line for better readability in the console output.
+        # print()
+    
+    # Return the list of text contents of all paragraph tags.
+    return conList
+
 
 if __name__ == "__main__":
     headers = {
