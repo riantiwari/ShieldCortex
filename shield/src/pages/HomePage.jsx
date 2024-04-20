3431830762f25f93451bdff507ec57b9b3331ad9
jeffjoyal <jeff.joyal04@gmail.com>
2024-04-20 16:20:32 -0400

    front end file structure update

---
+++ shield/src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function HomePage() {
+  return (
+    <div>HomePage
+        <h1> This will hold our message and shit</h1>
+    </div>
+  )
+}
+
+export default HomePage
---
+++ shield/src/pages/MapPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function MapPage() {
+  return (
+    <div>MapPage
+        <h1> this will have the actual map interface </h1>
+    </div>
+  )
+}
+
+export default MapPage
