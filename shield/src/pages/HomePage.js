fef4845a018da07850aa451a549f99686d6fe322
jeffjoyal <jeff.joyal04@gmail.com>
2024-04-21 04:07:11 -0400

    GOOD EDIT
    I updated a lot, finally got the map working, markers will be placed with corresponding info windows that pop up. All that we need is for the data to be inputted in now.

--- shield/src/pages/HomePage.js
+++ shield/src/pages/HomePage.js
@@ -1,130 +1,126 @@
-import React from 'react'
+import React, { useState} from 'react'
 
 import { Link } from 'react-router-dom'
 
-import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
+import { GoogleMap, useLoadScript, MarkerF, InfoWindowF, AdvancedMarkerF} from '@react-google-maps/api';
 
-import ControlPanel from './control-panel';
-import {MovingMarker} from './moving-marker';
-import {MarkerWithInfowindow} from './marker-with-infowindow';
+
+
+const latitudeInput = 40.0;
+const longitudeInput = -75.0;
 
 const libraries = ['places'];
 const mapContainerStyle = {
   width: '100vw',
   height: '100vh',
 };
+// D.C. coordinates
 const center = {
   lat: 38.9072, // default latitude
   lng: -77.0369, // default longitude
 };
 
-const API_KEY =
-  globalThis.GOOGLE_MAPS_API_KEY ?? (process.env.GOOGLE_MAPS_API_KEY);
+const testInput = {
+  lat: latitudeInput,
+  lng: longitudeInput,
+}
+
+const icons = {
+  marker: "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png"
+}
 
 
-const HomePage = () => {
-  return(
-  <APIProvider apiKey={API_KEY} libraries={['marker']}>
-    <Map
-      mapId={'bf51a910020fa25a'}
-      defaultZoom={3}
-      defaultCenter={{lat: 12, lng: 0}}
-      gestureHandling={'greedy'}
-      disableDefaultUI>
-      {/* simple marker */}
-      <Marker
-        position={{lat: 10, lng: 10}}
-        clickable={true}
-        onClick={() => alert('marker was clicked!')}
-        title={'clickable google.maps.Marker'}
-      />
-
-      {/* advanced marker with customized pin */}
-      <AdvancedMarker
-        position={{lat: 20, lng: 10}}
-        title={'AdvancedMarker with customized pin.'}>
-        <Pin
-          background={'#22ccff'}
-          borderColor={'#1e89a1'}
-          glyphColor={'#0f677a'}></Pin>
-      </AdvancedMarker>
-
-      {/* advanced marker with html pin glyph */}
-      <AdvancedMarker
-        position={{lat: 15, lng: 20}}
-        title={'AdvancedMarker with customized pin.'}>
-        <Pin background={'#22ccff'} borderColor={'#1e89a1'} scale={1.4}>
-          {/* children are rendered as 'glyph' of pin */}
-          👀
-        </Pin>
-      </AdvancedMarker>
-
-      {/* advanced marker with html-content */}
-      <AdvancedMarker
-        position={{lat: 30, lng: 10}}
-        title={'AdvancedMarker with custom html content.'}>
-        <div
-          style={{
-            width: 16,
-            height: 16,
-            position: 'absolute',
-            top: 0,
-            left: 0,
-            background: '#1dbe80',
-            border: '2px solid #0e6443',
-            borderRadius: '50%',
-            transform: 'translate(-50%, -50%)'
-          }}></div>
-      </AdvancedMarker>
-
-      {/* simple positioned infowindow */}
-      <InfoWindow position={{lat: 40, lng: 0}} maxWidth={200}>
-        <p>
-          This is the content for another infowindow with <em>HTML</em>
-          -elements.
-        </p>
-      </InfoWindow>
-
-      {/* continously updated marker */}
-      <MovingMarker />
-
-      {/* simple stateful infowindow */}
-      <MarkerWithInfowindow />
-    </Map>
-    <ControlPanel />
-  </APIProvider>
-  );
-};
-
-export default HomePage
-{/*
+function HomePage(){
+  // checks if aps loaded in properly
   const { isLoaded, loadError } = useLoadScript({
     googleMapsApiKey: 'AIzaSyCE2zGGFYmasHDNeJiFXzqtCyvoDs4IjOs',
     libraries,
   });
 
+  // functions to toggle the info window that displays marker information
+  const [isInfoWindowOpen, setIsInfoWindowOpen] = useState(new Array(2).fill(false));
+  const toggleInfoWindow = (index) => {
+    setIsInfoWindowOpen(prevState => {
+      const newState = [...prevState];
+      newState[index] = !newState[index];
+      return newState;
+    });
+  };
+
   if (loadError) {
     return <div>Error loading maps</div>;
   }
-
   if (!isLoaded) {
     return <div>Loading maps</div>;
   }
+
+
 
   return (
     <div>
       <div>
         <h1>Welcome to ShieldCortex: Defense Contracting Mapper</h1>
-        <h1>Doglinfd</h1>
-        <h1> practice this is jeff's edit</h1>
-        <h2> practice this is jack's edit</h2>
+        <h1>Im a big fat gay boy</h1>
+        <h1> My names jeff and I like boys </h1>
+        <h2> Im a big dick lover yes plz papa </h2>
       </div>
       <GoogleMap
         mapContainerStyle={mapContainerStyle}
         zoom={10}
         center={center}
       >
-        <Marker position={center} />
+
+        {/* this is going to be the test marker for a test set of input coordinates */}
+        {/* MARKER 1 */}
+        <MarkerF 
+          position={testInput}
+          icon={{
+            url: icons.marker,
+            scaledSize: new window.google.maps.Size(50, 50), // Adjust the size if needed
+          }}
+          // onClick={() => alert("Latitude: " + testInput.lat + "\nLongitude: " + testInput.lng)}
+          onClick={() => toggleInfoWindow(0)}
+          title={'Philly test marker'}
+        />
+        {/* this is the corresponding infowindow that will pop up when the corresponding marker is clicked */}
+        {isInfoWindowOpen[0] && (
+          <InfoWindowF position={testInput} onCloseClick={() => toggleInfoWindow(0)}>
+            <div>
+              <h1 className='location'>Philadelphia</h1> 
+              <h2 className='companyName'> Lockheed Martin </h2>
+              <h2 className='branch'> Army Branch</h2>
+              <p className='contractInfo'> 
+                test info about this goofy ahh contract
+              </p>
+            </div>
+          </InfoWindowF>
+        )}
+
+
+        {/* MARKER 2 */}
+        <MarkerF 
+          position={center}
+          icon={{
+            url: icons.marker,
+            scaledSize: new window.google.maps.Size(50, 50), // Adjust the size if needed
+          }}
+          onClick={() => toggleInfoWindow(1)}
+          title={'DC test marker'}
+        />
+        {/* this is the corresponding infowindow that will pop up when the corresponding marker is clicked */}
+        {isInfoWindowOpen[1] && (
+          <InfoWindowF position={center} onCloseClick={() => toggleInfoWindow(1)}>
+            <div>
+              <h1 className='location'> Washington D.C.</h1> 
+              <h2 className='companyName'> Northrup Rummond </h2>
+              <h2 className='branch'> Navy Seal Branch bih</h2>
+              <p className='contractInfo'> 
+                yessuhhhhh more info yessuh
+              </p>
+            </div>
+          </InfoWindowF>
+        )}
+
       </GoogleMap>
     </div>
   );
@@ -132,7 +128,7 @@ export default HomePage
 
 export default HomePage;
 
-*/}
+
 
 {/*
 function HomePage() {
