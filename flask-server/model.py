26a8a3675f8a3e24fb16f4bc5947ad187a42b08c
RianTiwari1 <64282367+riantiwari@users.noreply.github.com>
2024-04-20 22:12:22 -0400

    Storing csv data

---
+++ flask-server/model.py
@@ -0,0 +1,20 @@
+import csv
+              
+def main():
+    contracts = load_data("contracts.csv")
+    contracts = [','.join(contract) for contract in contracts]
+    for contract in contracts:
+        print(contract) 
+
+def load_data(filename):
+    contracts = []
+    with open(filename, newline='') as file: 
+        reader = csv.reader(file, delimiter=",")
+        next(reader)  
+        for row in reader:
+            if row:  
+                contracts.append(row)
+    return contracts
+
+if __name__ == "__main__":
+    main()
